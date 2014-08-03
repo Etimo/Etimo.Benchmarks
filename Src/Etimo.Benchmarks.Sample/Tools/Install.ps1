b93a14a003d57d20c15ee395f7af00d414feda64
Joakim Olesen <joakim.olesen@etimo.se>
2014-08-03 21:47:43 +0200

    Added Install.ps1

---
+++ Src/Etimo.Benchmarks.Sample/Tools/Install.ps1
@@ -0,0 +1,5 @@
+﻿param($installPath, $toolsPath, $package, $project)
+ 
+$dataFile = $project.ProjectItems.Item("Content").Item("Etimo.Benchmarks").Item("Data").ProjectItems.Item("The World Bank - GDP Per Capita.csv")
+ 
+$dataFile.Properties.Item("CopyToOutputDirectory").Value = 1
